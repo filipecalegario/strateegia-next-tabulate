f2faa6a37308d8f610afa20c5de8fbe7600f0494
filipecalegario <fcac@cin.ufpe.br>
2022-05-22 10:54:11 -0300

    add dummy data

---
+++ data/dummyData.js
@@ -0,0 +1,44 @@
+export const dummyData = [
+    {
+        "Id": "0x00001",
+        "Autor": "Autor 1",
+        "Questão": "Como é seu nome?",
+        "Resposta": "Filipe Calegario"
+    },
+    {
+        "Id": "0x00001",
+        "Autor": "Autor 1",
+        "Questão": "Qual a sua altura?",
+        "Resposta": "178"
+    },
+    {
+        "Id": "0x00002",
+        "Autor": "Autor 2",
+        "Questão": "Como é seu nome?",
+        "Resposta": "Alissa Seixas"
+    },
+    {
+        "Id": "0x00002",
+        "Autor": "Autor 2",
+        "Questão": "Qual a sua altura?",
+        "Resposta": "168"
+    },
+    {
+        "Id": "0x00001",
+        "Autor": "Autor 1",
+        "Questão": "Onde você mora?",
+        "Resposta": "Recife"
+    },
+    {
+        "Id": "0x00002",
+        "Autor": "Autor 2",
+        "Questão": "Onde você mora?",
+        "Resposta": "Recife"
+    },
+    {
+        "Id": "0x00002",
+        "Autor": "Autor 2",
+        "Questão": "Onde você mora?",
+        "Resposta": "Nos Aflitos"
+    }
+];
